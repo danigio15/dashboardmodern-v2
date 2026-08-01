b27167e1ae641fdfa18b2b1f19c977f4c7b8d391
danigio15 <152004596+danigio15@users.noreply.github.com>
2026-08-01 06:11:35 +0200

    test: fix manifest path

--- custom_components/dashboardmodern/frontend/tests/release-0152-version.test.js
+++ custom_components/dashboardmodern/frontend/tests/release-0152-version.test.js
@@ -2,7 +2,7 @@ import assert from "node:assert/strict";
 import { readFile } from "node:fs/promises";
 import test from "node:test";
 
-const manifestUrl = new URL("../../../manifest.json", import.meta.url);
+const manifestUrl = new URL("../../manifest.json", import.meta.url);
 
 test("the corrective release is version 0.14.12", async () => {
   const manifest = JSON.parse(await readFile(manifestUrl, "utf8"));
