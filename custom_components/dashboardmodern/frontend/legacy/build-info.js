e813151324e84b14b860c96bc1af36a3d6fbafdc
danigio15 <152004596+danigio15@users.noreply.github.com>
2026-08-10 17:10:14 +0200

    chore: release v1.0.0-beta.8

--- custom_components/dashboardmodern/frontend/legacy/build-info.js
+++ custom_components/dashboardmodern/frontend/legacy/build-info.js
@@ -3,8 +3,8 @@ import "../src/sections/beta-entry-section.js";
 
 export const BUILD_INFO = Object.freeze({
   generated: false,
-  integrationVersion: "1.0.0-beta.7",
-  dashboardVersion: "1.0.0-beta.7",
+  integrationVersion: "1.0.0-beta.8",
+  dashboardVersion: "1.0.0-beta.8",
   moduleVersion: 14,
   schemaVersion: 4,
   date: "UNBUILT",
